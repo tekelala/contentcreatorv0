0010595b868b36e689c1d2009c525402f6cc999e
tekelala <camilo@tekelala.com>
2023-05-01 10:42:59 -0500

    step by step creator v0

--- app.py
+++ app.py
@@ -13,17 +13,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         return "API key for OpenAI not found. Please set the 'OPENAI_API_KEY' environment variable."
 
     openai.api_key = openai_api_key
-    
-    messages = [
-        {
-            "role":"system",
-            "content": "You are a the editor of a well knwon business magazine. Writting with a business thinker articles for a business audience, to help them have a better life and unleashing their potential"
-        },
-        {   "role": "user", 
-            "content": "Help the writter in around 1000 words: {prompt}"
-            
-        }
-    ]
+    messages = [{"role": "user", "content": prompt}]
         
     response = openai.ChatCompletion.create(
         model=model,
@@ -35,25 +25,36 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 # Streamlit application
 def app():
     # Title of the application
-    st.title("Content Creator with GPT-3.5-turbo")
+    st.title("Content Creator, please provide the taks you want to get the step by step guide")
 
     # Input field for the user to enter a topic
-    topic = st.text_input("Enter a topic:")
+    tasks = st.text_input("Enter a task:")
 
     # Where the magic happens, the prompt
     
+    role_prompt = """Nice and insightful professor as Andrew Ng
+    """
+    
+    goal_prompt = """to give an step by step guide on how leverage on AI tools \
+    to be more efficient give your readers more time to focus on the things \
+    that matter and use much better their human capabilities
+    """
+    
+    prompt = f"""
+    You are ```{role_prompt}``` and your goal is ```{goal_prompt}``` to perform with the assistance of AI tool the following ```{tasks}``` in 1000 words.
+    """
 
     # Button to generate content
     if st.button("Create"):
         # If the input field is not empty, get the content from GPT-3.5-turbo
-        if topic:
+        if tasks:
             with st.spinner('Generating content...'):
                 # Call the function to get the completion
-                content = get_completion(topic)
+                content = get_completion(prompt)
                 # Display the generated content
                 st.write(content)
         else:
-            st.error("Please enter a topic.")
+            st.error("Please enter a task.")
 
 # Run the application
 if __name__ == "__main__":
