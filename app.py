d73c64f5dc76ba81cc2a816522469bff63903466
tekelala <camilo@tekelala.com>
2023-05-01 10:23:35 -0500

    fixing

--- app.py
+++ app.py
@@ -41,23 +41,7 @@ def app():
     topic = st.text_input("Enter a topic:")
 
     # Where the magic happens, the prompt
-    text = f"""
-You should express what you want a model to do by \ 
-providing instructions that are as clear and \ 
-specific as you can possibly make them. \ 
-This will guide the model towards the desired output, \ 
-and reduce the chances of receiving irrelevant \ 
-or incorrect responses. Don't confuse writing a \ 
-clear prompt with writing a short prompt. \ 
-In many cases, longer prompts provide more clarity \ 
-and context for the model, which can lead to \ 
-more detailed and relevant outputs.
-"""
-topic = f"""
-Summarize the text delimited by triple backticks \ 
-into a single sentence.
-```{text}```
-"""
+    
 
     # Button to generate content
     if st.button("Create"):
