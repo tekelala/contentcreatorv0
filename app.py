37efa15f1659c1d78e3e2a8bd4d04d4179728be0
tekelala <camilo@tekelala.com>
2023-05-01 09:33:54 -0500

    First version

---
+++ app.py
@@ -0,0 +1,41 @@
+# Importing required libraries
+import openai
+import streamlit as st
+import os
+
+# Function to get the model completion
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    # Set the OpenAI API key from the environment variable
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+# Streamlit application
+def app():
+    # Title of the application
+    st.title("Content Creator with GPT-3.5-turbo")
+
+    # Input field for the user to enter a topic
+    topic = st.text_input("Enter a topic:")
+
+    # Button to generate content
+    if st.button("Create"):
+        # If the input field is not empty, get the content from GPT-3.5-turbo
+        if topic:
+            with st.spinner('Generating content...'):
+                # Call the function to get the completion
+                content = get_completion(topic)
+                # Display the generated content
+                st.write(content)
+        else:
+            st.error("Please enter a topic.")
+
+# Run the application
+if __name__ == "__main__":
+    app()
